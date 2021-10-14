dbb74d2b855b6ca960977718593a5bf90cf956f8
Michael Towson <MTowson@gmail.com>
2021-10-13 17:45:31 -0700

    add api functionality

--- app.js
+++ app.js
@@ -4,23 +4,57 @@ var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 //ROUTES
 weatherApp.config(function ($routeProvider) {
   $routeProvider
-
   .when('/', {
     templateUrl: 'views/home.htm',
     controller: 'homeController'
   })
 
-  .when('/forecast', {
-    templateUrl: 'views/forecast.htm',
-    controller: 'forecastController',
+  .when('/weather', {
+    templateUrl: 'views/weather.htm',
+    controller: 'weatherController',
   })
 })
 
-//CONTROLLERS
-weatherApp.controller('homeController', ['$scope', function($scope) {
+//SERVICES
 
+//--cityService
+weatherApp.service('cityService', function() {
+  this.city = "Sacramento";
+})
+
+//CONTROLLERS
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+  $scope.city = cityService.city;
+  $scope.$watch('city', function() {
+    cityService.city = $scope.city;
+  })
 }])
 
-weatherApp.controller("forecastController", ["$scope", function ($scope) {
+weatherApp.controller("weatherController", ["$scope", '$resource', 'cityService', function ($scope, $resource, cityService) {
+  $scope.city = cityService.city;
 
-}]);
+  //Get current weather
+  $scope.currentWeatherAPI = $resource(
+    "https://api.openweathermap.org/data/2.5/weather?q="+$scope.city+"&appid=e6e26f422db591152cb877a0a947da10",
+    {
+      callback: "JSON_CALLBACK",
+    },
+    { get: { method: "JSONP" } }
+  );
+  $scope.weatherResult = $scope.currentWeatherAPI.get() 
+  console.log($scope.weatherResult)
+
+  //Get weather forecast
+    $scope.weatherForecastAPI = $resource(
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+        $scope.city +
+        "&appid=e6e26f422db591152cb877a0a947da10",
+      {
+        callback: "JSON_CALLBACK",
+      },
+      { get: { method: "JSONP" } }
+    );
+    $scope.weatherForecastResult = $scope.weatherForecastAPI.get();
+    console.log($scope.weatherForecastResult);
+  }
+]);
