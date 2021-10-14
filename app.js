37c4e7f6a081b3091d7bf45ab01d48689517ec48
Michael Towson <MTowson@gmail.com>
2021-10-14 15:11:15 -0700

    finish base functionality

--- app.js
+++ app.js
@@ -35,26 +35,38 @@ weatherApp.controller("weatherController", ["$scope", '$resource', 'cityService'
 
   //Get current weather
   $scope.currentWeatherAPI = $resource(
-    "https://api.openweathermap.org/data/2.5/weather?q="+$scope.city+"&appid=e6e26f422db591152cb877a0a947da10",
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      $scope.city +
+      "&appid=e6e26f422db591152cb877a0a947da10",
     {
       callback: "JSON_CALLBACK",
     },
     { get: { method: "JSONP" } }
   );
-  $scope.weatherResult = $scope.currentWeatherAPI.get() 
-  console.log($scope.weatherResult)
+  $scope.weatherResult = $scope.currentWeatherAPI.get();
+  console.log($scope.weatherResult);
 
   //Get weather forecast
-    $scope.weatherForecastAPI = $resource(
-      "https://api.openweathermap.org/data/2.5/forecast?q=" +
-        $scope.city +
-        "&appid=e6e26f422db591152cb877a0a947da10",
-      {
-        callback: "JSON_CALLBACK",
-      },
-      { get: { method: "JSONP" } }
-    );
-    $scope.weatherForecastResult = $scope.weatherForecastAPI.get();
-    console.log($scope.weatherForecastResult);
+  $scope.weatherForecastAPI = $resource(
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      $scope.city +
+      "&appid=e6e26f422db591152cb877a0a947da10",
+    {
+      callback: "JSON_CALLBACK",
+    },
+    { get: { method: "JSONP" } }
+  );
+  $scope.weatherForecastResult = $scope.weatherForecastAPI.get();
+  console.log($scope.weatherForecastResult);
+
+  //Convert to Fahrenheit
+  $scope.convertToFahrenheit = function (degK) {
+    return Math.round(1.8 * (degK - 273) + 32);
+  };
+
+  //Convert to Date
+  $scope.convertToDate = function(dt) {
+    return new Date(dt * 1000);
   }
+}
 ]);
